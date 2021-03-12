02df5758bb8236f4e5a3fd80940e7aa71170ef56
kumaradoss-quantum <76904568+kumaradoss-quantum@users.noreply.github.com>
2021-03-12 18:22:57 +0530

    Resume

---
+++ Resume1/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__, template_folder='template')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/download')
+def download():
+    return send_file('resume5.pdf', as_attachment=True)
+
+
+app.run(debug=True)
